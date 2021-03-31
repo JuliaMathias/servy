b15af71c92490ada7587ef3a6e3483d27626fd11
JuliaMathias <juliamss@gmail.com>
2021-03-31 18:37:41 -0300

    Add http request and response

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,5 +4,19 @@ defmodule Servy.Handler do
   This module handles Servy
   """
 
+  request = """
+  GET /wildthings HTTP/1.1
+  Host: example.com
+  User-Agent: ExampleBrowser/1.0
+  Accept: */*
 
+  """
+
+  expected_response = """
+  HTTP/1.1 200 OK
+  Content-Type: text/html
+  Content-Length: 20
+
+  Bears, Lions, Tigers
+  """
 end
