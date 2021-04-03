78d6c4b528f547d3cc65ed14ae2f64c02ab86c52
JuliaMathias <juliamss@gmail.com>
2021-04-03 18:49:49 -0300

    Add inspectors to parser module

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -3,13 +3,23 @@ defmodule Servy.Parser do
   alias Servy.Conv
 
   def parse(request) do
+    IO.inspect(label: "original request")
+
     [top, params_string] = String.split(request, "\n\n")
+    IO.inspect(top, label: "top")
+    IO.inspect(params_string, label: "params_string")
 
     [request_line | header_lines] = String.split(top, "\n")
+    IO.inspect(request_line, label: "request_line")
+    IO.inspect(header_lines, label: "header_lines")
 
     [method, path, _] = String.split(request_line, " ")
+    IO.inspect(method, label: "method")
+    IO.inspect(path, label: "path")
 
     params = parse_params(params_string)
+
+    IO.inspect(params, label: "params")
 
     %Conv{
        method: method,
