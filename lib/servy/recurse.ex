ee20b72d5371a4e37ad2410f79252209d8c95848
JuliaMathias <juliamss@gmail.com>
2021-04-07 14:22:33 -0300

    Triple the lisit given (exercise 14.2)

--- lib/servy/recurse.ex
+++ lib/servy/recurse.ex
@@ -5,6 +5,15 @@ defmodule Recurse do
   end
 
   def sum([], total), do: total
+
+  def triple([head|tail]) do
+    [head*3 | triple(tail)]
+  end
+
+  def triple([]), do: []
+
+
 end
 
 IO.puts Recurse.sum([1, 2, 3, 4, 5], 0)
+IO.puts Recurse.triple([1, 2, 3, 4, 5])
