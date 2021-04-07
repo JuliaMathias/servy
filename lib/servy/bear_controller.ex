791b8fe23b3a04df5cf6600f2e419c98df92eb07
JuliaMathias <juliamss@gmail.com>
2021-04-07 16:20:44 -0300

    Create bear_item()

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -3,6 +3,10 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
+  defp bear_item(bear) do
+    "<li>#{bear.name} - #{bear.type}</li>"
+  end
+
   def index(conv) do
     items =
       Wildthings.list_bears()
