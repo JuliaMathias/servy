65bfc2629f64c79af0c82b27b985b6b42fa65924
JuliaMathias <juliamss@gmail.com>
2021-04-08 18:51:36 -0300

    Add handler test

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -3,30 +3,21 @@ defmodule Servy.Parser do
   alias Servy.Conv
 
   def parse(request) do
-    # IO.inspect(request, label: "original request")
+    [top, params_string] = String.split(request, "\r\n\r\n")
 
-    [top, params_string] = String.split(request, "\n\n")
-    IO.inspect(top, label: "top")
-    IO.inspect(params_string, label: "params_string")
-
-    [request_line | header_lines] = String.split(top, "\n")
-    IO.inspect(request_line, label: "request_line")
-    IO.inspect(header_lines, label: "header_lines")
+    [request_line | header_lines] = String.split(top, "\r\n")
 
     [method, path, _] = String.split(request_line, " ")
-    IO.inspect(method, label: "method")
-    IO.inspect(path, label: "path")
 
     headers = parse_headers(header_lines, %{})
 
     params = parse_params(headers["Content-Type"], params_string)
 
-    IO.inspect(params, label: "params")
-
     %Conv{
        method: method,
        path: path,
-       params: params
+       params: params,
+       headers: headers
      }
   end
 
