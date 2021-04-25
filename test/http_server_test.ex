a7bd4addfb47537de2053470952d88fd75302f6b
JuliaMathias <juliamss@gmail.com>
2021-04-25 15:23:45 -0300

    Refactor HttpServerTest to call on multiple urls

--- test/http_server_test.ex
+++ test/http_server_test.ex
@@ -6,10 +6,17 @@ defmodule HttpServerTest do
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
 
-    url = "http://localhost:4000/wildthings"
+    urls = [
+      "http://localhost:4000/wildthings",
+      "http://localhost:4000/sensors",
+      "http://localhost:4000/kaboom",
+      "http://localhost:4000/hibernate",
+      "http://localhost:4000/api/bears",
+      "http://localhost:4000/about"
+    ]
 
-    1..5
-    |> Enum.map(fn _ -> Task.async(fn -> HTTPoison.get(url) end) end)
+    urls
+    |> Enum.map(fn _ -> Task.async(fn -> HTTPoison.get(&1) end) end)
     |> Enum.map(&Task.await/1)
     |> Enum.map(&assert_successful_response/1)
   end
