0a6ba2a898906fb09a12a240ec2f408732ad244d
JuliaMathias <juliamss@gmail.com>
2021-05-18 18:33:35 -0300

    Use supervisor in kickstarter.rx

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -1,9 +1,9 @@
 defmodule Servy.KickStarter do
   use GenServer
 
-  def start do
-    IO.puts "Starting the kickstarter..."
-    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  def start_link(_arg) do
+    IO.puts("Starting the kickstarter...")
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
@@ -13,40 +13,15 @@ defmodule Servy.KickStarter do
   end
 
   def handle_info({:EXIT, _pid, reason}, _state) do
-    IO.puts "HttpServer exited (#{inspect reason})"
+    IO.puts("HttpServer exited (#{inspect(reason)})")
     server_pid = start_server()
     {:noreply, server_pid}
   end
 
   defp start_server do
-    IO.puts "Starting the HTTP server..."
+    IO.puts("Starting the HTTP server...")
     server_pid = spawn_link(Servy.HttpServer, :start, [4000])
     Process.register(server_pid, :http_server)
     server_pid
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
